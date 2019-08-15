b8d0cc2fe7fb8282e0bbd5e6f30066301ca8f080
r03ert0 <rto@braincatalogue.org>
2019-08-15 19:50:51 +0200

    update compiling script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-gcc -Wall main.c -o meshparam
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  gcc -Wall main.c -o meshparam_linux
+elif [[ "$OSTYPE"  == "darwin18" ]]; then
+  gcc -Wall main.c -o meshparam_mac
+fi

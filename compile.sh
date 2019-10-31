e1da0a40bcecc6e62e3f7ea63d6faab6a6dc1fd9
roberto <rto@pasteur.fr>
2019-10-31 20:22:54 +0100

    handle darwin/linux compilation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-gcc -Wall main.c -o meshparam
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  gcc -Wall main.c -o meshparam_linux -lm
+elif [[ "$OSTYPE" == "darwin" ]]; then
+  gcc -Wall main.c -o meshparam_mac
+fi
